579d60bd3ec1b127ff7721d36fd219bbea1ab532
Wonseok Yang <wsyang@digicaps.com>
2024-02-21 17:59:09 +0900

    add user CRUD test all.

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,13 +2,79 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/yangoneseok/voyager/util"
 )
 
 func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.Role, user2.Role)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	arg := UpdateUserParams{
+		ID:       user1.ID,
+		Username: util.RandomName(),
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+	require.NoError(t, err)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, arg.Username, user2.Username)
+	require.Equal(t, user1.Role, user2.Role)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestDeleteUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	err := testQueries.DeleteUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, user2)
+}
+
+func TestListUsers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomUser(t)
+	}
+	arg := ListUsersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	users, err := testQueries.ListUsers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, users, 5)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+}
+
+// only for Test
+func createRandomUser(t *testing.T) User {
 	arg := CreateUserParams{
 		Username: util.RandomName(),
 		Email:    util.RandomEmail(),
@@ -25,4 +91,5 @@ func TestCreateUser(t *testing.T) {
 
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
+	return user
 }
