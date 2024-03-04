c13a90307f74af5642f9af96efa4cad7d4f2f6c0
Wonseok Yang <wsyang@digicaps.com>
2024-03-04 18:10:11 +0900

    add token maker interface and payloat struct

---
+++ db/token/payload.go
@@ -0,0 +1,29 @@
+package token
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    int64     `json:"user_id"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates a new token payload with a specific userId and duration
+func NewPayload(userID int64, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	payload := &Payload{
+		ID:        tokenID,
+		UserID:    userID,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.6
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/google/uuid v1.4.0
 	github.com/jackc/pgx/v5 v5.5.3
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.18.2
